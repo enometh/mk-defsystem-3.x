b17b27b0a346ddbcdffa3b4c11d386ab87311806
Madhu <enometh@net.meer>
2020-01-22 11:04:45 +0530

    file local variables

--- defsystem.lisp
+++ defsystem.lisp
@@ -585,6 +585,9 @@
 ;;;
 ;;; 2020-01-18 dsm  system-relative-pathname hack
 ;;;
+;;; 2020-01-22 dsm  export *file-local-variables* bind it around
+;;;                 operations on :file and :private-file.
+;;;
 
 ;;;---------------------------------------------------------------------------
 ;;; ISI Comments
@@ -4467,6 +4470,9 @@ In these cases the name of the output file is of the form
 	))))
 
 
+(defvar *file-local-variables* nil)
+(export '*file-local-variables*)
+
 (defun operate-on-component (component operation force &aux changed)
   ;; Returns T if something changed and had to be compiled.
   (let ((type (component-type component))
@@ -4562,7 +4568,9 @@ In these cases the name of the output file is of the form
 	  (setq changed
 		(case type
 		  ((:file :private-file)
-		   (funcall (component-operation operation) component force))
+		   (progv *file-local-variables*
+		       (mapcar #'symbol-value *file-local-variables*)
+		     (funcall (component-operation operation) component force)))
 		  ((:module :system :subsystem :defsystem)
 		   (operate-on-components component operation force changed))))
 
