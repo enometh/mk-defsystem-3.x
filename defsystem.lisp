a48c95207a0124e6432122422c4447f635cf7b87
Madhu <enometh@net.meer>
2026-09-01 09:29:11 +0530

    fixup! more mkcl fixes

--- defsystem.lisp
+++ defsystem.lisp
@@ -7207,7 +7207,7 @@ nil)
 					     (car components)))
 			   (setq path (cdr path)))
 			  (t (return (car components)))))
-		   (t (setq components (cdr components)))))))))
+		   (t (setq components (cdr components)))))))))))
 ||#
 
 (defun system-relative-pathname (system path)
